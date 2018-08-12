1cdb2c0b2712cb4bd90110e5541852088ffbab9a
Ana Rebollo Pin <ana.rebollo.pin@gmail.com>
2018-08-12 15:41:08 +0200

    provisional commit

--- RandomUser/DataLayer/NetworkManagement.swift
+++ RandomUser/DataLayer/NetworkManagement.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+class NetworkManagement {
+    
+    var url = URL(string: "https://api.randomuser.me/?results=40")!
+    lazy var configuration: URLSessionConfiguration = URLSessionConfiguration.default
+    lazy var session: URLSession = URLSession(configuration: self.configuration)
+    typealias JSONDictionaryHandler = (([String:AnyObject]?) -> Void)
+    
+    func downloadJSONFromURL(_completion: @escaping JSONDictionaryHandler) -> Set<User>
+    {
+        var randomUsers = Set<User>()
+        let request = URLRequest(url: self.url)
+        let dataTask = session.dataTask(with: request)
+        {(data, response,error) in
+            if error == nil {
+                if let httpResponse = response as? HTTPURLResponse {
+                    switch httpResponse.statusCode {
+                    case 200:
+                        if let data = data {
+                            do {
+                                let callResult = try JSONDecoder().decode(RandomAPIResult.self, from:data)
+                                for user in callResult.results {
+                                    let user = self.mapDTOInModel(user: user)
+                                    randomUsers.insert(user)
+                                }
+                            } catch let error as NSError {
+                                print ("Error processing json data: \(error.description)")
+                            }
+                        }
+                    default:
+                        print("HTTP Response Code: \(httpResponse.statusCode)")
+                    }
+                }
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        dataTask.resume()
+        return randomUsers
+    }
+    
+    func mapDTOInModel(user:RandomAPIUser) -> User {
+        let currentUser = User(name: user.name.first, surname: user.name.last,
+                               email: user.email, picture: user.picture.medium.absoluteString,
+                               phone: user.cell, gender: user.gender,
+                               registerDate: user.registered.date,
+                               location: Location(street: user.location.street,
+                                                  city: user.location.city,
+                                                  state: user.location.state))
+        return currentUser
+    }
+}
--- RandomUser/View/ViewController.swift
+++ RandomUser/View/ViewController.swift
@@ -14,15 +14,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var randomUsers = Set<User>()
     var selectedUser: User?
     
-    var url = URL(string: "https://api.randomuser.me/?results=40")!
-    lazy var configuration: URLSessionConfiguration = URLSessionConfiguration.default
-    lazy var session: URLSession = URLSession(configuration: self.configuration)
-    typealias JSONDictionaryHandler = (([String:AnyObject]?) -> Void)
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        downloadJSONFromURL(_completion: { (data) in })
+        var domainCall = DownloadRamdomUser()
+        self.randomUsers = domainCall.downloadUsers()
+        DispatchQueue.main.async { [unowned self] in
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,50 +46,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let detailVC = storyboard?.instantiateViewController(withIdentifier: "detailVC") as! DetailViewController
         detailVC.user = selectedUser
         navigationController?.pushViewController(detailVC, animated: false)
-    }
-    
-    func mapDTOInModel(user:RandomAPIUser) {
-        let currentUser = User(name: user.name.first, surname: user.name.last,
-                            email: user.email, picture: user.picture.medium.absoluteString,
-                            phone: user.cell, gender: user.gender,
-                            registerDate: user.registered.date,
-                            location: Location(street: user.location.street,
-                                               city: user.location.city,
-                                               state: user.location.state))
-        
-        self.randomUsers.insert(currentUser)
-        DispatchQueue.main.async { [unowned self] in
-            self.tableView.reloadData()
-        }
-    }
-    
-    func downloadJSONFromURL(_completion: @escaping JSONDictionaryHandler)
-    {
-        let request = URLRequest(url: self.url)
-        let dataTask = session.dataTask(with: request)
-        {(data, response,error) in
-            if error == nil {
-                if let httpResponse = response as? HTTPURLResponse {
-                    switch httpResponse.statusCode {
-                    case 200:
-                        if let data = data {
-                            do {
-                                let callResult = try JSONDecoder().decode(RandomAPIResult.self, from:data)
-                                for user in callResult.results {
-                                    self.mapDTOInModel(user: user)
-                                }
-                            } catch let error as NSError {
-                                print ("Error processing json data: \(error.description)")
-                            }
-                        }
-                    default:
-                        print("HTTP Response Code: \(httpResponse.statusCode)")
-                    }
-                }
-            } else {
-                print("Error: \(error?.localizedDescription)")
-            }
-        }
-        dataTask.resume()
     }
 }
